28f7cce860d97d4ee989b5aac48ff8afee49fff2
yanwenhao687 <ywh_687@163.com>
2020-02-11 10:22:39 +0800

    YES...

--- src/com/yanwenhao/bean/GithubTest.java
+++ src/com/yanwenhao/bean/GithubTest.java
@@ -4,7 +4,6 @@ public class GithubTest {
 
 	public static void main(String[] args) {
 		System.out.println("Egit1...");
-		System.out.println("回退历史版本后提交,需要修改代码");
-		System.out.println("jjjj");
+		System.out.println("YES...");
 	}
 }
