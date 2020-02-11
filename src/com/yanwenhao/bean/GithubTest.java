21ab1038d58cdc5286b0df9a4de298e324c7d34b
yanwenhao687 <ywh_687@163.com>
2020-02-11 08:46:53 +0800

    jjjj

--- src/com/yanwenhao/bean/GithubTest.java
+++ src/com/yanwenhao/bean/GithubTest.java
@@ -5,5 +5,6 @@ public class GithubTest {
 	public static void main(String[] args) {
 		System.out.println("Egit1...");
 		System.out.println("回退历史版本后提交,需要修改代码");
+		System.out.println("jjjj");
 	}
 }
